40edb7174fc4b18d2ae711527abed38bc52d5d21
Mihira Jayasekera <mihira@tinymachine.net>
2021-05-13 23:12:58 -0700

    feat: add sorting options

--- src/index.js
+++ src/index.js
@@ -16,14 +16,36 @@ const iosColors = Object.keys(iosColorsRaw).map((name) => {
   }
 })
 
-const allHexColors = [
-  ...iosColors.map(({ hex }) => hex),
-  ...figmaColors.map(({ hex }) => hex),
-]
+const allHexColors = {
+  iosFirst: [
+    ...iosColors.map(({ hex }) => hex),
+    ...figmaColors.map(({ hex }) => hex),
+  ],
+  figmaFirst: [
+    ...figmaColors.map(({ hex }) => hex),
+    ...iosColors.map(({ hex }) => hex),
+  ],
+}
 
-const uniqueHexColors = [
-  ...new Set(allHexColors.map((hex) => hex.toLowerCase())),
-]
+const uniqueHexColorsSortedBy = {
+  ios: [
+    ...new Set(
+      allHexColors.iosFirst.map((hex) => hex.toLowerCase())
+    ),
+  ],
+  figma: [
+    ...new Set(
+      allHexColors.figmaFirst.map((hex) => hex.toLowerCase())
+    ),
+  ],
+  color: [
+    ...new Set(
+      allHexColors.iosFirst
+        .map((hex) => hex.toLowerCase())
+        .sort()
+    ),
+  ],
+}
 
 const getColorDot = (hex) => `
   <span
@@ -49,24 +71,10 @@ const getMatchingColorNames = ({ colorSet, hex }) => {
     .filter((color) => color.hex.toLowerCase() === hex)
     .map(({ name }) => name)
 
-  return matches.length > 0 ? matches : [`<span class="error">no match</span>`]
+  return matches.length > 0
+    ? matches
+    : [`<span class="error">no match</span>`]
 }
-
-const markup = uniqueHexColors.map(
-  (hex) => `
-    <tr>
-      <td class="mono">${getHexMarkup(hex)}</td>
-      <td class="mono small">${getMatchingColorNames({
-        colorSet: iosColors,
-        hex,
-      }).join('<br>')}</td>
-      <td class="mono small">${getMatchingColorNames({
-        colorSet: figmaColors,
-        hex,
-      }).join('<br>')}</td>
-    </tr>
-  `
-)
 
 document.getElementById('app').innerHTML = `
 <main>
@@ -74,14 +82,57 @@ document.getElementById('app').innerHTML = `
   <table>
     <thead>
       <tr>
-        <th>Color <span class="small">(ignoring alpha)</span></th>
-        <th>iOS Color Styles</th>
-        <th>Figma Color Styles</th>
+        <th data-sort="color">Color <span class="small">(ignoring alpha)</span></th>
+        <th data-sort="ios">iOS Color Styles</th>
+        <th data-sort="figma">Figma Color Styles</th>
       </tr>
     </thead>
     <tbody id="markup-container">
-      ${markup.join('')}
     </tbody>
   </table>
 </main>
 `
+
+const markupContainer = document.querySelector(
+  '#markup-container'
+)
+
+const getAndInsertMarkup = (hexSet) => {
+  const markup = hexSet.map(
+    (hex) => `
+      <tr>
+        <td class="mono">${getHexMarkup(hex)}</td>
+        <td class="mono small">${getMatchingColorNames({
+          colorSet: iosColors,
+          hex,
+        }).join('<br>')}</td>
+        <td class="mono small">${getMatchingColorNames({
+          colorSet: figmaColors,
+          hex,
+        }).join('<br>')}</td>
+      </tr>
+    `
+  )
+
+  markupContainer.innerHTML = markup.join('')
+}
+
+const toggles = document.querySelectorAll('[data-sort]')
+
+const sortColorsBy = (sort) => {
+  toggles.forEach((toggle) => {
+    toggle.classList.remove('active')
+  })
+  const toggle = document.querySelector(`[data-sort="${sort}"]`)
+  toggle.classList.add('active')
+
+  getAndInsertMarkup(uniqueHexColorsSortedBy[sort])
+}
+
+toggles.forEach((toggle) => {
+  toggle.addEventListener('click', () =>
+    sortColorsBy(toggle.dataset.sort)
+  )
+})
+
+sortColorsBy('ios')
