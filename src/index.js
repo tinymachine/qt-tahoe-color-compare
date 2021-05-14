95931558ff029765b443348046a2e8aa8a1bee4e
Mihira Jayasekera <mihira@tinymachine.net>
2021-05-14 00:49:54 -0700

    tweak: improve Figma color styles formatting

--- src/index.js
+++ src/index.js
@@ -1,37 +1,53 @@
-import "./styles.css";
-import figmaColorsRaw from "../data/figma-colors";
-import iosColorsRaw from "../data/ios-colors";
+import './styles.css'
+import figmaColorsRaw from '../data/figma-colors'
+import iosColorsRaw from '../data/ios-colors'
+
+// finesse colors
 
 const figmaColors = figmaColorsRaw.map(({ name, hex }) => {
   return {
-    name: name.replace(/ /g, ""),
-    hex: hex.toLowerCase()
-  };
-});
+    name: name
+      .substring(2)
+      .replace(/ /g, '')
+      .replace(/---/g, '/')
+      .replace(/\//g, `<span class="subtle padded">/</span>`)
+      .replace(/\s-/g, ' ')
+      .replace(/--/g, '-'),
+    hex: hex.toLowerCase(),
+  }
+})
 
 const iosColors = Object.keys(iosColorsRaw).map((name) => {
   return {
     name,
-    hex: iosColorsRaw[name].any.slice(0, -2).toLowerCase() // strip alpha from end
-  };
-});
+    hex: iosColorsRaw[name].any.slice(0, -2).toLowerCase(), // strip alpha from end
+  }
+})
+
+// set up array of colors
 
 const allHexColors = {
   iosFirst: [
     ...iosColors.map(({ hex }) => hex),
-    ...figmaColors.map(({ hex }) => hex)
+    ...figmaColors.map(({ hex }) => hex),
   ],
   figmaFirst: [
     ...figmaColors.map(({ hex }) => hex),
-    ...iosColors.map(({ hex }) => hex)
-  ]
-};
+    ...iosColors.map(({ hex }) => hex),
+  ],
+}
 
 const uniqueHexColorsSortedBy = {
   ios: [...new Set(allHexColors.iosFirst.map((hex) => hex))],
-  figma: [...new Set(allHexColors.figmaFirst.map((hex) => hex))],
-  color: [...new Set(allHexColors.iosFirst.map((hex) => hex).sort())]
-};
+  figma: [
+    ...new Set(allHexColors.figmaFirst.map((hex) => hex)),
+  ],
+  color: [
+    ...new Set(allHexColors.iosFirst.map((hex) => hex).sort()),
+  ],
+}
+
+// generate markup
 
 const getColorDot = (hex) => `
   <span
@@ -39,28 +55,32 @@ const getColorDot = (hex) => `
     style="background-color: ${hex};"
   >
   </span>
-`;
+`
 
 const styleHash = (hex) => `
   ${hex.replace(/#/, '<span class="subtle">#</span>')}
-`;
+`
 
 const getHexMarkup = (hex) => `
   <span class="hex-with-dot">
     ${getColorDot(hex)}
     <span>${styleHash(hex)}</span>
   </span>
-`;
+`
 
 const getMatchingColorNames = ({ colorSet, hex }) => {
   const matches = colorSet
     .filter((color) => color.hex === hex)
-    .map(({ name }) => name);
+    .map(({ name }) => name)
 
-  return matches.length > 0 ? matches : [`<span class="error">no match</span>`];
-};
+  return matches.length > 0
+    ? matches
+    : [`<span class="error">no match</span>`]
+}
 
-document.getElementById("app").innerHTML = `
+// set up document structure
+
+document.getElementById('app').innerHTML = `
 <main>
   <h1>Tahoe Colors Compared</h1>
   <table>
@@ -75,9 +95,13 @@ document.getElementById("app").innerHTML = `
     </tbody>
   </table>
 </main>
-`;
+`
 
-const markupContainer = document.querySelector("#markup-container");
+// set up toggles
+
+const markupContainer = document.querySelector(
+  '#markup-container'
+)
 
 const getAndInsertMarkup = (hexSet) => {
   const markup = hexSet.map(
@@ -86,33 +110,35 @@ const getAndInsertMarkup = (hexSet) => {
         <td class="mono">${getHexMarkup(hex)}</td>
         <td class="mono small">${getMatchingColorNames({
           colorSet: iosColors,
-          hex
-        }).join("<br>")}</td>
+          hex,
+        }).join('<br>')}</td>
         <td class="mono small">${getMatchingColorNames({
           colorSet: figmaColors,
-          hex
-        }).join("<br>")}</td>
+          hex,
+        }).join('<br>')}</td>
       </tr>
     `
-  );
+  )
 
-  markupContainer.innerHTML = markup.join("");
-};
+  markupContainer.innerHTML = markup.join('')
+}
 
-const toggles = document.querySelectorAll("[data-sort]");
+const toggles = document.querySelectorAll('[data-sort]')
 
 const sortColorsBy = (sort) => {
   toggles.forEach((toggle) => {
-    toggle.classList.remove("active");
-  });
-  const toggle = document.querySelector(`[data-sort="${sort}"]`);
-  toggle.classList.add("active");
+    toggle.classList.remove('active')
+  })
+  const toggle = document.querySelector(`[data-sort="${sort}"]`)
+  toggle.classList.add('active')
 
-  getAndInsertMarkup(uniqueHexColorsSortedBy[sort]);
-};
+  getAndInsertMarkup(uniqueHexColorsSortedBy[sort])
+}
 
 toggles.forEach((toggle) => {
-  toggle.addEventListener("click", () => sortColorsBy(toggle.dataset.sort));
-});
+  toggle.addEventListener('click', () =>
+    sortColorsBy(toggle.dataset.sort)
+  )
+})
 
-sortColorsBy("ios");
+sortColorsBy('ios')
