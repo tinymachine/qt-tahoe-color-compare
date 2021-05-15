8139803445bf64a6d406322822b81370a98ef163
Mihira Jayasekera <mihira@tinymachine.net>
2021-05-15 12:57:11 -0700

    Unsure again what updated...

--- src/colorData.js
+++ src/colorData.js
@@ -1,5 +1,20 @@
-import figmaColorsRaw from '../data/figma-colors'
+import figmaColorsRawTxt from '../data/figma-colors.txt'
 import iosColorsRaw from '../data/ios-colors'
+
+/**
+ * convert Figma CSS variables exported from
+ * 'Export styles to CSS variables' Figma plugin
+ * to JSON
+ */
+const figmaColorsRaw = figmaColorsRawTxt
+  .split('\n')
+  .filter((line) => line && line.substring(0, 2) !== '/*')
+  .map((line) => {
+    return {
+      name: line.split(': ')[0],
+      hex: line.split(': ')[1].slice(0, -1),
+    }
+  })
 
 export const figmaColors = figmaColorsRaw.map(
   ({ name, hex }) => {
